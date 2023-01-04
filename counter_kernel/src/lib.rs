bb95106905bcfc5329272a471eb1e2efd3731d35
lykimq <lykimq@gmail.com>
2023-01-04 14:08:42 +0700

    Format: add copyright headers

--- counter_kernel/src/lib.rs
+++ counter_kernel/src/lib.rs
@@ -1,27 +1,8 @@
-/*****************************************************************************/
-/*                                                                           */
-/* Open Source License                                                       */
-/* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                */
-/*                                                                           */
-/* Permission is hereby granted, free of charge, to any person obtaining a   */
-/* copy of this software and associated documentation files (the "Software"),*/
-/* to deal in the Software without restriction, including without limitation */
-/* the rights to use, copy, modify, merge, publish, distribute, sublicense,  */
-/* and/or sell copies of the Software, and to permit persons to whom the     */
-/* Software is furnished to do so, subject to the following conditions:      */
-/*                                                                           */
-/* The above copyright notice and this permission notice shall be included   */
-/* in all copies or substantial portions of the Software.                    */
-/*                                                                           */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*/
-/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  */
-/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   */
-/* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*/
-/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   */
-/* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       */
-/* DEALINGS IN THE SOFTWARE.                                                 */
-/*                                                                           */
-/*****************************************************************************/
+// SPDX-FileCopyrightText: 2022 TriliTech <contact@trili.tech>
+// SPDX-FileCopyrightText: 2022 Nomadic Labs <contact@nomadic-labs.com>
+// SPDX-FileCopyrightText: 2022 Marigold <contact@marigold.dev>
+//
+// SPDX-License-Identifier: MIT
 
 use anyhow::{ensure, Result};
 use debug::debug_msg;
@@ -43,17 +24,17 @@ pub struct Counter {
 }
 
 impl Counter {
-    /// Create a new counter
+    // Create a new counter
     pub fn new(val: i8) -> Self {
         Self { val }
     }
 
-    /// Public read-only method: Returns the counter value
+    // Public read-only method: Returns the counter value
     pub fn get_num(&self) -> i8 {
         self.val
     }
 
-    /// Increment the counter
+    // Increment the counter
     pub fn increment(&mut self) -> i8 {
         self.val += 1;
         return self.val;
@@ -64,7 +45,7 @@ impl Counter {
         return self.val;
     }
 
-    /// Reset the counter to 0
+    // Reset the counter to 0
     pub fn reset(&mut self) -> i8 {
         self.val = 0;
         return self.val;
--- output_kernel/src/lib.rs
+++ output_kernel/src/lib.rs
@@ -1,29 +1,10 @@
-/*****************************************************************************/
-/*                                                                           */
-/* Open Source License                                                       */
-/* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                */
-/*                                                                           */
-/* Permission is hereby granted, free of charge, to any person obtaining a   */
-/* copy of this software and associated documentation files (the "Software"),*/
-/* to deal in the Software without restriction, including without limitation */
-/* the rights to use, copy, modify, merge, publish, distribute, sublicense,  */
-/* and/or sell copies of the Software, and to permit persons to whom the     */
-/* Software is furnished to do so, subject to the following conditions:      */
-/*                                                                           */
-/* The above copyright notice and this permission notice shall be included   */
-/* in all copies or substantial portions of the Software.                    */
-/*                                                                           */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*/
-/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  */
-/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   */
-/* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*/
-/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   */
-/* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       */
-/* DEALINGS IN THE SOFTWARE.                                                 */
-/*                                                                           */
-/*****************************************************************************/
+// SPDX-FileCopyrightText: 2022 TriliTech <contact@trili.tech>
+// SPDX-FileCopyrightText: 2022 Nomadic Labs <contact@nomadic-labs.com>
+// SPDX-FileCopyrightText: 2022 Marigold <contact@marigold.dev>
+//
+// SPDX-License-Identifier: MIT
 
-/* Use this kernel with test unit and using the mode mock_host only */
+//! Use this kernel with test unit and using the mode mock_host only
 
 #![allow(dead_code)]
 #![allow(unused_imports)]
@@ -44,9 +25,8 @@ const MAX_READ_INPUT_SIZE: usize = if MAX_INPUT_MESSAGE_SIZE > MAX_INPUT_SLOT_DA
     MAX_INPUT_SLOT_DATA_CHUNK_SIZE
 };
 
-/* Kernel:
-    This kernel read input and write output to both the kernel output and log
-*/
+// Kernel: This kernel read input and write output to both the kernel output and log
+
 pub fn test_output_run<Host: RawRollupCore>(host: &mut Host) {
     match host.read_input(MAX_READ_INPUT_SIZE) {
         Ok(Some(Input::Slot(message @ SlotData { level, id, .. }))) => {

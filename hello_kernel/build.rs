5109c67462e03b542e8c4ca5bcfde0ea80adb453
Daniel Hines <d4hines@gmail.com>
2023-03-17 14:23:23 -0400

    remove the hello_kernel
    It primarily focuses on capn proto,
    which is not strictly needed, and more
    of an implementation choice.

--- hello_kernel/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    if let Err(err) = capnpc::CompilerCommand::new()
-        .file("schema/hello.capnp")
-        .run()
-    {
-        panic!("capnpc: {err:?}")
-    }
-}
--- hello_kernel/schema/hello.capnp
+++
@@ -1,5 +0,0 @@
-@0x9a3d7b05e0d21c87;
-
-struct SayHello {
-    name @0 :Text;
-}
